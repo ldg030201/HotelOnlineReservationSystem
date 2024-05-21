4879dc7ccdb334e80751a6527afb0e865f0839d4
ldg <ldg030201>
2024-05-21 22:40:13 +0900

    init

---
+++ src/main/java/com/tuk/hors/HotelOnlineReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tuk.hors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelOnlineReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelOnlineReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuk/hors/HotelOnlineReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuk.hors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelOnlineReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
